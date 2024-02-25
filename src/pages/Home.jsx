2f2fa6cb2d42ccf3d0e7476bfac339ca73d56da3
Buse <busengokler@gmail.com>
2024-02-25 21:39:06 +0300

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  // Lookup a single random meal
+  // www.themealdb.com/api/json/v1/1/random.php
 
-export default Home
+  const [randomMeal, setRandomMeal] = useState(null);
+  const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+  useEffect(() => {
+    const fetchRandomMeal = async () => {
+      try {
+        const response = await axios.get(randomMealUrl);
+        const data = response.data;
+        setRandomMeal(data.meals[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchRandomMeal();
+  }, []);
+  const clipPathStyle = {
+    clipPath:"polygon(0% 0%, 0% 100%, 25% 100%, 25% 25%, 75% 25%, 75% 75%, 25% 75%, 25% 100%, 100% 100%, 100% 0%)",
+  };
+  return (
+    <div className="bg-gray-100">
+      <div>
+        {randomMeal == null ? (
+          <p>Not found</p>
+        ) : (
+          <div className="flex justify-between mx-auto">
+            <h2 
+                className="text-5xl"
+            >
+                {randomMeal.strMeal}
+            </h2>
+
+            <img
+             style={clipPathStyle}
+              src={randomMeal.strMealThumb}
+              alt="randomImg"
+              className="w-80 h-80"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
