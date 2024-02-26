1c93256c3a9ecfd0bf5dbb3820b795b74a04326b
Buse <busengokler@gmail.com>
2024-02-26 16:26:49 +0300

    home design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,71 +1,98 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import SearchMeal from "../components/SearchMeal";
 
 const Home = () => {
+  const [area, setArea] = useState(null);
   const [randomMeal, setRandomMeal] = useState(null);
-  const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+  const [categories, setCategories] = useState(null);
+
+  const URL = "https://www.themealdb.com/api/json/v1/1";
+  const areaUrl = `${URL}/list.php?a=list`;
+  const randomMealUrl = `${URL}/random.php`;
+  const categoriesUrl = `${URL}/categories.php`;
 
   useEffect(() => {
-    const fetchRandomMeal = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get(randomMealUrl);
-        const data = response.data;
-        setRandomMeal(data.meals[0]);
+        // Fetch random meal
+        const randomMealResponse = await axios.get(randomMealUrl);
+        const randomMealData = randomMealResponse.data;
+        setRandomMeal(randomMealData.meals[0]);
+
+        // Fetch categories
+        const categoriesResponse = await axios.get(categoriesUrl);
+        const categoriesData = categoriesResponse.data;
+        setCategories(categoriesData.categories);
+
+        //Fetch area
+        const areaResponse = await axios.get(areaUrl);
+        const areaData = areaResponse.data;
+        setArea(areaData.meals);
       } catch (error) {
         console.error(error);
       }
     };
-    fetchRandomMeal();
+    fetchData();
   }, []);
-  // https://www.themealdb.com/api/json/v1/1/categories.php
 
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 mx-3">
       <div>
-        {randomMeal == null ? (
-          <p>Not found</p>
-        ) : (
-          <div>
-            <hr />
-            <div className="flex">
-              <div className="rounded-full p-4 bg-red-300">category1</div>
-              <div className="rounded-full p-4 bg-red-300">category1</div>
-              <div className="rounded-full p-4 bg-red-300">category1</div>
-              <div className="rounded-full p-4 bg-red-300">category1</div>
-              <div className="rounded-full p-4 bg-red-300">category1</div>
-              <div className="rounded-full p-4 bg-red-300">category1</div>
-              <div className="rounded-full p-4 bg-red-300">category1</div>
-            </div>
-            <hr />
-            <div className="grid grid-cols-2">
-              <div className="relative col-span-1">
-                <h2 className="absolute bg-yellow-300 text-xl font-semibold bottom-0">
+        {randomMeal == null ? (<p>Not found</p>) : ( 
+        <div>
+          <hr />
+          <div className="grid grid-cols-7 gap-2">
+            {categories && categories.map((item) => (
+              <div
+                key={item.idCategory}
+                className="flex flex-col items-center"
+              >
+                <img
+                  src={item.strCategoryThumb}
+                  alt={item.strCategory}
+                  className="w-20 h-20 object-cover rounded-full"
+                />
+                <div className="rounded-full">{item.strCategory}</div>
+              </div>
+            ))}
+          </div>
+          <hr />
+
+          <div className="grid grid-cols-3 mx-auto">
+            <div className="relative col-span-1">
+              <div
+                className="absolute bg-opacity-50 bg-orange-100 
+                bottom-0 m-3 p-2 w-64 h-32 overflow-y-hidden"
+              >
+                <h2 className="text-lg font-semibold mb-2">
                   {randomMeal.strMeal}
                 </h2>
-
+                <p>{randomMeal.strCategory}</p>
+                <p>{randomMeal.strArea}</p>
+              </div>
+              <div className="m-3">
                 <img
                   src={randomMeal.strMealThumb}
                   alt="randomImg"
-                  className="w-64 h-64"
+                  className="w-96 h-72 rounded-md"
                 />
-              </div>
-              <div className="col-span-1">
-                <div className="bg-red-300 p-4 m-3">description</div>
-                <div className="bg-red-300 p-4 m-3">description</div>
-                <div className="bg-red-300 p-4 m-3">description</div>
               </div>
             </div>
 
-            <div>
-              <div className="w-full absolute">
-                <SearchMeal />
-              </div>
+            <div className="col-span-2 flex flex-wrap text-center">
+              {area && area.map((item, index) => (
+                <div className="w-28 h-16" key={index}>
+                  <div className="bg-red-300 m-1 my-3 p-2 rounded-md">
+                    {item.strArea}
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
+  </div>
   );
 };
 
