97e23e01d9ee4095a293456589f7c531c24ffbe7
Buse <busengokler@gmail.com>
2024-02-26 19:57:34 +0300

    meal item

--- src/components/Meals/Meals.jsx
+++ src/components/Meals/Meals.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import SearchMeal from '../SearchMeal'
+import MealItem from './MealItem'
 
 const Meals = () => {
   return (
-    <div>Meals</div>
+    <div>
+      <SearchMeal />
+      <div>
+        <MealItem />
+      </div>
+    </div>
   )
 }
 
