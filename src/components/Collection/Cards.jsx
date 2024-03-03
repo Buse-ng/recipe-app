057fc84da6bfa265886e07f5fa2809db069d2896
Buse <busengokler@gmail.com>
2024-03-03 19:14:51 +0300

    cards design and delete

--- src/components/Collection/Cards.jsx
+++ src/components/Collection/Cards.jsx
@@ -1,54 +1,112 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CollectionContext } from "../../context/CollectionContext";
 import { Link } from "react-router-dom";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Cards = () => {
+  const { id, recipes, setRecipes } = useContext(CollectionContext);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
-  const {id, recipes} = useContext(CollectionContext);
-  
+  const deleteRecipe = (id) => {
+    const newRecipes = recipes.filter((item) => {
+      return item.id !== id;
+    });
+    setRecipes(newRecipes);
+    setIsOpenModal(false);
+  };
+
+  const openDeletModal = () => {setIsOpenModal(true);}
+  const closeDeletModal = () => {setIsOpenModal(false);}
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-sm md:max-w-none mx-auto md:mx-0">
+    <div
+      className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 
+      xl:grid-cols-5 gap-8 max-w-sm md:max-w-none mx-auto"
+    >
       {recipes.map((item) => (
-        <div className="max-w-xs border rounded-lg shadow bg-gray-200 border-gray-100" key={item.id}>
-          <Link to={`/recipes/${id}`}>
-            <img
-              className="rounded-t-lg"
-              src={item.img}
-              alt="image"
-            />
-          </Link>
-          <div className="p-5">
-            <Link to={`/recipes/${id}`}>
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        <div
+          key={item.id}
+          className="max-w-xs border rounded-lg 
+          bg-gray-200 border-gray-100 my-auto"
+        >
+          <div className="relative">
+            <Link to={`/recipes/${item.id}`}>
+              <img
+                className="rounded-t-lg object-cover w-full h-44"
+                src={item.img}
+                alt="image"
+              />
+            </Link>
+          </div>
+
+          <div className="p-3 text-gray-900">
+            <Link to={`/recipes/${item.id}`}>
+              <h5
+                className="text-lg mb-2 md:text-2xl font-bold 
+              tracking-tight truncate"
+              >
                 {item.title}
               </h5>
             </Link>
-            <p className="mb-3 font-normal text-gray-700">
-              {item.category}
-            </p>
+
             <div className="flex justify-between">
-              <p>{item.time} min</p>
+              <p className="mb-3 text-gray-600">{item.category}</p>
+              <p>{item.total_time} min</p>
+            </div>
+
+            <div
+              className="flex justify-between items-center 
+              text-sm md:text-base"
+            >
               <Link
-                  to={`/recipes/${id}`}
-                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                to={`/recipes/${item.id}`}
+                className="inline-flex items-center px-3 py-2 
+                font-medium text-center text-white 
+                rounded-lg bg-green-700 hover:bg-green-800"
               >
-                  Detail
-                  <svg
-                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                  >
-                  <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                  </svg>
+                View Recipe
               </Link>
+
+              <button
+                onClick={openDeletModal}
+                className="flex items-center justify-center 
+                rounded-lg bg-gray-300 p-2"
+              >
+                <FaTrashAlt className="text-gray-500 hover:text-red-600" />
+              </button>
+              {/*delete modal*/}
+              {isOpenModal && (
+                <div className={`${isOpenModal ? "flex" : "hidden"} 
+                overflow-y-auto overflow-x-hidden fixed top-0 right-0 
+                left-0 z-50 justify-center items-center w-full 
+                md:inset-0 h-[calc(100%-1rem)] max-h-full`}
+                >
+                  <div className="p-4 w-full max-w-md max-h-full">
+                    <div
+                      className="p-4 md:p-5 text-center rounded-lg 
+                      shadow bg-gray-300 text-white text-sm"
+                    >
+                      <h3 className="mb-5 text-lg font-semibold text-gray-700">
+                        Are you sure you want to delete this recipe?
+                      </h3>
+                      <button
+                        className="bg-red-600 hover:bg-red-700 font-medium 
+                        rounded-lg inline-flex items-center px-5 py-2.5"
+                        onClick={() => deleteRecipe(item.id)}
+                      >
+                        Yes, I'm sure
+                      </button>
+                      <button
+                        className="py-2.5 px-5 ms-3 font-medium rounded-lg 
+                        border bg-gray-700 border-gray-600 hover:bg-gray-800"
+                        onClick={closeDeletModal}
+                      >
+                        No, cancel
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
