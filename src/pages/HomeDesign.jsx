a98f47430129cb020a60bfacf55594558061ca4a
Buse <busengokler@gmail.com>
2024-03-10 23:11:01 +0300

    home responsive

--- src/pages/HomeDesign.jsx
+++ src/pages/HomeDesign.jsx
@@ -1,9 +1,6 @@
-import axios from "axios";
-import { useContext, useEffect, useState } from "react";
-import { MealsContext } from "../context/MealsContext";
 import { Link } from "react-router-dom";
-import { GiHotMeal, GiMeal } from "react-icons/gi";
-import { FaArrowRight, FaMapLocationDot } from "react-icons/fa6";
+import { GiHotMeal } from "react-icons/gi";
+import { FaMapLocationDot } from "react-icons/fa6";
 import { MdArrowForwardIos } from "react-icons/md";
 import a from "../assets/a.jpg";
 
@@ -13,14 +10,15 @@ const HomeDesign = () => {
       className="flex items-center justify-start w-full 
       h-screen bg-cover bg-no-repeat bg-center bg-[url('assets/a.jpg')]"
     >
-      <div className="text-gray-600 text-start p-2 text-xl m-6">
-        <h2 className="text-5xl font-bold mb-6">
+      {/* lg:max-w-lg xl:max-w-xl */}
+      <div className="text-gray-600 text-start p-2 text-xs md:text-xl m-6 overflow-ellipsis overflow-hidden">
+        <h2 className="text-base lg:text-5xl font-bold mb-6">
           Master the Art of Cooking:
-          <span className="block text-3xl my-2">
+          <span className="block lg:text-3xl my-2">
             Discover, Create, and Savor!
           </span>
         </h2>
-        <div className="flex flex-col text-gray-400 text-sm ">
+        <div className="hidden md:flex flex-col text-gray-400 text-sm">
           Push the boundaries of flavor! Explore unique recipes with Recipe App
           in the kitchen.
           <span>
@@ -28,11 +26,11 @@ const HomeDesign = () => {
             await you.
           </span>
         </div>
-        <div className="flex justify-between items-center mt-16">
-          <div className="font-semibold flex gap-16">
+        <div className="flex flex-col md:flex-row justify-between items-center md:mt-16">
+          <div className="font-semibold flex gap-4 md:gap-16">
             <div className="mb-4 flex flex-col tracking-wide ">
               <div className="flex gap-2 items-center">
-                <span className="text-3xl text-[#D60324]">
+                <span className="md:text-3xl text-[#D60324]">
                   <GiHotMeal />
                 </span>
                 300+
@@ -42,7 +40,7 @@ const HomeDesign = () => {
 
             <div className="mb-4 flex flex-col tracking-wide ">
               <div className="flex gap-2 items-center">
-                <span className="text-3xl text-[#D60324]">
+                <span className="md:text-3xl text-[#D60324]">
                   <FaMapLocationDot />
                 </span>
                 28
@@ -51,13 +49,13 @@ const HomeDesign = () => {
             </div>
           </div>
           <div
-            className="bg-[#D60324] hover:bg-[#DC0213] text-base 
-            p-2 rounded-xl font-semibold w-36 cursor-pointer animate-pulse"
+            className="bg-[#D60324] hover:bg-[#DC0213] md:text-base 
+            p-2 rounded-xl font-semibold md:w-36 cursor-pointer animate-pulse"
           >
-            <Link to="/recipes" className="">
-              <p className="flex items-center justify-center gap-4  text-gray-100">
+            <Link to="/recipes">
+              <p className="flex items-center justify-center md:gap-4 text-gray-100">
                 See More
-                <span className="">
+                <span>
                   <MdArrowForwardIos />
                 </span>
               </p>
