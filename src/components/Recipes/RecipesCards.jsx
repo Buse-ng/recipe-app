46a12be4e38e807401dbbaa7e83edb99cd34d530
Buse <busengokler@gmail.com>
2024-03-17 20:43:51 +0300

    added no result

--- src/components/Recipes/RecipesCards.jsx
+++ src/components/Recipes/RecipesCards.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import RecipesItem from "./RecipesItem";
 import { RecipesContext } from "../../context/RecipesContext";
 import axios from "axios";
+import NoResultSvg from "./NoResultSvg";
 
 const RecipesCards = () => {
   const { 
@@ -43,21 +44,9 @@ const RecipesCards = () => {
         </div>
       ) 
       : (
-        // default recipes : I choose "starter meals"
-        <div>
-          <h2 className="my-8 text-xl font-semibold mb-2">
-            Starter Meals
-          </h2>
-          <div 
-            className="max-w-sm md:max-w-none mx-auto md:mx-0 
-            grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
-          >
-            {defaultMeals && defaultMeals.map((item) => (
-              <div key={item.idMeal}>
-                <RecipesItem item={item} />
-              </div>
-            ))}
-          </div>
+        <div className="flex flex-col items-center justify-center mt-16">
+          <p className="logoname md:text-4xl font-semibold text-gray-500">No results found!</p>
+          <NoResultSvg />
         </div>
         )
       }
